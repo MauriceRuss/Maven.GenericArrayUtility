1c0b98c213552f85cafc000e56788eddc1d7ccec
Mo Russ <mauriceruss302@gmail.com>
2020-03-08 18:38:58 -0400

    CountDuplicatesInMergeTests passed

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,40 @@
 package com.zipcodewilmington.arrayutility;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T> {
+    private T[] array;
+
+    public ArrayUtility(T[] array) {
+        this.array = array;
+    }
+
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T value){
+        int numberOfDuplicates=0;
+        T[] both = ArrayUtils.addAll(array, arrayToMerge);
+        for ( int i = 0 ; i < both.length ; i++) {
+            if (both[i].equals(value)) {
+                numberOfDuplicates++;
+            }
+        }
+        return numberOfDuplicates;
+    }
+
+    public Integer getNumberOfOccurrences(T value){
+        return null;
+    }
+
+    public T getMostCommonFromMerge(T[] array){
+        return null;
+    }
+
+    public T[] removeValue(T value){
+        //list.remove(value);
+        //return list.toArray(new T[0]);
+        return null;
+    }
+
 }
