e7f4bd1dbeea8ffffe60786db9265e8873c5b51c
Mo Russ <mauriceruss302@gmail.com>
2020-03-08 19:35:08 -0400

    cleanedcountDuplicatesInMerge and getNumberOfOccurrences with method  countNumberOfOccurrences getMostCommonFromMerges tests passed

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -2,33 +2,67 @@ package com.zipcodewilmington.arrayutility;
 
 import org.apache.commons.lang3.ArrayUtils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by leon on 3/6/18.
  */
 public class ArrayUtility<T> {
-    private T[] array;
+
+    private final T[] array;
 
     public ArrayUtility(T[] array) {
         this.array = array;
     }
 
-    public Integer countDuplicatesInMerge(T[] arrayToMerge, T value){
+    private Integer countNumberOfOccurrences(T[] array, T value) {
         int numberOfDuplicates=0;
-        T[] both = ArrayUtils.addAll(array, arrayToMerge);
-        for ( int i = 0 ; i < both.length ; i++) {
-            if (both[i].equals(value)) {
+        for ( int i = 0 ; i < array.length ; i++) {
+            if (array[i].equals(value)) {
                 numberOfDuplicates++;
             }
         }
         return numberOfDuplicates;
     }
 
-    public Integer getNumberOfOccurrences(T value){
-        return null;
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T value){
+        return countNumberOfOccurrences(ArrayUtils.addAll(array, arrayToMerge),value);
     }
 
-    public T getMostCommonFromMerge(T[] array){
-        return null;
+    public Integer getNumberOfOccurrences(T value){
+        return countNumberOfOccurrences(array,value);
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge){
+        /**
+         *Creating HasMap form merger array.
+         * use the value of hashmap to count the number of occurances
+         * compare number of occurrances for highest
+         * return key with the highest value
+         */
+        HashMap<T, Integer> common = new HashMap<>();
+        T[] both = ArrayUtils.addAll(array, arrayToMerge);
+        for(int i = 0; i < both.length; i ++){
+            if(common.containsKey(both[i])){
+                Integer count = common.get(both[i]);
+                count++;
+                common.put(both[i],count);
+            }else{
+                common.put(both[i], 1);
+            }
+        }
+        T mostOcVal = null;
+        Integer highestNumOfOcc = 0;
+        for(Map.Entry<T, Integer> entry : common.entrySet()){
+            if(entry.getValue() >= highestNumOfOcc){
+                mostOcVal = entry.getKey();
+                highestNumOfOcc = entry.getValue();
+
+            }
+        }
+
+        return mostOcVal;
     }
 
     public T[] removeValue(T value){
